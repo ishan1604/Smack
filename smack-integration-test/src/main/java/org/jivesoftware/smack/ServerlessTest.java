0af723888a2c7916f9f5bced44b263ef017e30e4
Ishan Khanna <ishan1604@gmail.com>
2015-06-26 20:24:09 +0530

    Trying to log the tests

--- smack-integration-test/src/main/java/org/jivesoftware/smack/ServerlessTest.java
+++ smack-integration-test/src/main/java/org/jivesoftware/smack/ServerlessTest.java
@@ -1,5 +1,7 @@
 package org.jivesoftware.smack;
 
+import org.igniterealtime.smack.inttest.AbstractSmackLowLevelIntegrationTest;
+import org.igniterealtime.smack.inttest.Configuration;
 import org.igniterealtime.smack.inttest.SmackIntegrationTest;
 
 import javax.jmdns.JmDNS;
@@ -7,24 +9,28 @@ import javax.jmdns.ServiceEvent;
 import javax.jmdns.ServiceInfo;
 import javax.jmdns.ServiceListener;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Created by ishan on 25/06/15.
  */
-public class ServerlessTest {
+public class ServerlessTest extends AbstractSmackLowLevelIntegrationTest{
+
+    public static final String PRESENCE = "_presence._tcp.local.";
+
+    public ServerlessTest(Configuration configuration, String testRunId) {
+        super(configuration, testRunId);
+    }
 
     @SmackIntegrationTest
     public void simpleTest() throws IOException, InterruptedException {
-
+        LOGGER.info("Logger info is working");
         JmDNS jmdns = JmDNS.create();
-        jmdns.registerService(ServiceInfo.create("_presence._tcp.local.", "juliet@pronto._presence._tcp.local.", 5562, "Testing Service A"));
-        jmdns.registerService(ServiceInfo.create("_presence._tcp.local.", "romeo@doronto._presence._tcp.local.", 5563, "Testing Service B"));
-
-        jmdns.addServiceListener("_presence._tcp.local.", new ServiceListener() {
+        jmdns.addServiceListener(PRESENCE, new ServiceListener() {
             @Override
             public void serviceAdded(ServiceEvent event) {
-                System.out.println(event.getInfo().getName());
-
+                LOGGER.info(event.toString());
             }
 
             @Override
@@ -37,6 +43,13 @@ public class ServerlessTest {
 
             }
         });
+        jmdns.registerService(ServiceInfo.create("_presence._tcp.local.", "juliet@pronto._presence._tcp.local.", 5562, "Testing Service A"));
         Thread.sleep(10000);
+        jmdns.registerService(ServiceInfo.create("_presence._tcp.local.", "romeo@doronto._presence._tcp.local.", 5563, "Testing Service B"));
+        Thread.sleep(10000);
+        for (ServiceInfo si : jmdns.list(PRESENCE)) {
+            LOGGER.info(si.toString());
+        }
+        Thread.sleep(5000);
     }
 }
